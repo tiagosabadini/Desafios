8f1275c17479afe6519b9d38a4c80c659b173059
Vinicius Lima <paulo.vinicius.f.l@live.com>
2018-03-08 14:49:15 -0300

    Initial commit

---
+++ Desafio1/index.php
@@ -0,0 +1,12 @@
+<?php
+//https://www.urionlinejudge.com.br/judge/pt/problems/view/1003
+
+
+    $A = -30;
+    $B = 10;
+
+    $PROD = $A*$B;
+
+    echo"PROD: ".$PROD;
+
+?>
---
+++ Desafio2/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>O Maior</title>
+</head>
+<body>
+   
+   
+</body>
+</html>
+
+<?php
+   
+   $a = 70;
+   $b = 20;
+   $b= 85;
+   //$abs = 50;
+
+   $maior = ($a+$b+abs($a-$b))/2;
+
+
+
+   echo "E o maior ".$maior;
+
